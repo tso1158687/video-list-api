79fb0cabfdaf68b71c55810bc87913c2c854196f
jason zheng <tso1158687@gmail.com>
2024-02-15 15:55:48 +0800

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Video API')
+    .setDescription('Provides a list of videos and their metadata.')
+    .setVersion('1.0')
+    .addTag('videos')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
